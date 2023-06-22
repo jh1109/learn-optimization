d095eb62288e4925a4b5ebdabf82c625ef15fc59
Juhee Noh <nohjuhee227@gmail.com>
2023-06-22 23:55:36 +0900

    LEARN: 함수 컴포넌트의 재평가 과정 이해하기(162-163)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((showParagraph) => !showParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
